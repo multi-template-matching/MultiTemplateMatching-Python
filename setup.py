1b708081e473f83731e65f33d4f01a957ff609dc
Laurent Thomas <laurent132.thomas@laposte.net>
2019-07-16 16:47:10 +0200

    Update version and spacing

--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+	long_description = fh.read()
 
 setuptools.setup(
-    name="Multi-Template-Matching",
-    version="1.1",
-    author="Laurent Thomas",
-    author_email="laurent132.thomas@laposte.net",
-    description="Object-recognition in images using multiple templates",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+	name="Multi-Template-Matching",
+	version="1.2",
+	author="Laurent Thomas",
+	author_email="laurent132.thomas@laposte.net",
+	description="Object-recognition in images using multiple templates",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
 	keywords="object-recognition object-localization",
-    url="https://github.com/LauLauThom/MultiTemplateMatching-Python",
-    packages=["MTM"],
+	url="https://github.com/LauLauThom/MultiTemplateMatching-Python",
+	packages=["MTM"],
 	install_requires=[
-          'numpy',
+		  'numpy',
 		  'opencv-python',
 		  'scikit-image',
 		  'scipy'
-      ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+	  ],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
 		"Operating System :: OS Independent",
 		"Topic :: Scientific/Engineering :: Image Recognition"
-    ],
+	],
 )
