069e9a788c0af7bc7424b038581b446130df17d7
Laurent <laurent132.thomas@laposte.net>
2019-10-10 16:26:35 +0200

    Identation crap

--- MTM/NMS.py
+++ MTM/NMS.py
@@ -14,82 +14,82 @@ from __future__ import division, print_function # for compatibility with Py2
 import pandas as pd
 
 def Point_in_Rectangle(Point, Rectangle):
-	'''Return True if a point (x,y) is contained in a Rectangle(x, y, width, height)'''
-	# unpack variables
-	Px, Py = Point
-	Rx, Ry, w, h = Rectangle
+    '''Return True if a point (x,y) is contained in a Rectangle(x, y, width, height)'''
+    # unpack variables
+    Px, Py = Point
+    Rx, Ry, w, h = Rectangle
 
-	return (Rx <= Px) and (Px <= Rx + w -1) and (Ry <= Py) and (Py <= Ry + h -1) # simply test if x_Point is in the range of x for the rectangle 
+    return (Rx <= Px) and (Px <= Rx + w -1) and (Ry <= Py) and (Py <= Ry + h -1) # simply test if x_Point is in the range of x for the rectangle 
 
 
 def computeIoU(BBox1,BBox2):
-	'''
-	Compute the IoU (Intersection over Union) between 2 rectangular bounding boxes defined by the top left (Xtop,Ytop) and bottom right (Xbot, Ybot) pixel coordinates
-	Code adapted from https://www.pyimagesearch.com/2016/11/07/intersection-over-union-iou-for-object-detection/
-	'''
-	#print('BBox1 : ', BBox1)
-	#print('BBox2 : ', BBox2)
-	
-	# Unpack input (python3 - tuple are no more supported as input in function definition - PEP3113 - Tuple can be used in as argument in a call but the function will not unpack it automatically)
-	Xleft1, Ytop1, Width1, Height1 = BBox1
-	Xleft2, Ytop2, Width2, Height2 = BBox2
-	
-	# Compute bottom coordinates
-	Xright1 = Xleft1 + Width1  -1 # we remove -1 from the width since we start with 1 pixel already (the top one)
-	Ybot1	= Ytop1	 + Height1 -1 # idem for the height
+    '''
+    Compute the IoU (Intersection over Union) between 2 rectangular bounding boxes defined by the top left (Xtop,Ytop) and bottom right (Xbot, Ybot) pixel coordinates
+    Code adapted from https://www.pyimagesearch.com/2016/11/07/intersection-over-union-iou-for-object-detection/
+    '''
+    #print('BBox1 : ', BBox1)
+    #print('BBox2 : ', BBox2)
+    
+    # Unpack input (python3 - tuple are no more supported as input in function definition - PEP3113 - Tuple can be used in as argument in a call but the function will not unpack it automatically)
+    Xleft1, Ytop1, Width1, Height1 = BBox1
+    Xleft2, Ytop2, Width2, Height2 = BBox2
+    
+    # Compute bottom coordinates
+    Xright1 = Xleft1 + Width1  -1 # we remove -1 from the width since we start with 1 pixel already (the top one)
+    Ybot1    = Ytop1     + Height1 -1 # idem for the height
 
-	Xright2 = Xleft2 + Width2  -1
-	Ybot2	= Ytop2	 + Height2 -1
+    Xright2 = Xleft2 + Width2  -1
+    Ybot2    = Ytop2     + Height2 -1
 
-	# determine the (x, y)-coordinates of the top left and bottom right points of the intersection rectangle
-	Xleft  = max(Xleft1, Xleft2)
-	Ytop   = max(Ytop1, Ytop2)
-	Xright = min(Xright1, Xright2)
-	Ybot   = min(Ybot1, Ybot2)
-	
-	# Compute boolean for inclusion
-	BBox1_in_BBox2 = Point_in_Rectangle((Xleft1, Ytop1), BBox2) and Point_in_Rectangle((Xleft1, Ybot1), BBox2) and Point_in_Rectangle((Xright1, Ytop1), BBox2) and Point_in_Rectangle((Xright1, Ybot1), BBox2)
-	BBox2_in_BBox1 = Point_in_Rectangle((Xleft2, Ytop2), BBox1) and Point_in_Rectangle((Xleft2, Ybot2), BBox1) and Point_in_Rectangle((Xright2, Ytop2), BBox1) and Point_in_Rectangle((Xright2, Ybot2), BBox1) 
-	
-	# Check that for the intersection box, Xtop,Ytop is indeed on the top left of Xbot,Ybot
-	if BBox1_in_BBox2 or BBox2_in_BBox1:
-		#print('One BBox is included within the other')
-		IoU = 1
-	
-	elif Xright<Xleft or Ybot<Ytop : # it means that there is no intersection (bbox is inverted)
-		#print('No overlap')
-		IoU = 0 
-	
-	else:
-		# Compute area of the intersecting box
-		Inter = (Xright - Xleft + 1) * (Ybot - Ytop + 1) # +1 since we are dealing with pixels. See a 1D example with 3 pixels for instance
-		#print('Intersection area : ', Inter)
+    # determine the (x, y)-coordinates of the top left and bottom right points of the intersection rectangle
+    Xleft  = max(Xleft1, Xleft2)
+    Ytop   = max(Ytop1, Ytop2)
+    Xright = min(Xright1, Xright2)
+    Ybot   = min(Ybot1, Ybot2)
+    
+    # Compute boolean for inclusion
+    BBox1_in_BBox2 = Point_in_Rectangle((Xleft1, Ytop1), BBox2) and Point_in_Rectangle((Xleft1, Ybot1), BBox2) and Point_in_Rectangle((Xright1, Ytop1), BBox2) and Point_in_Rectangle((Xright1, Ybot1), BBox2)
+    BBox2_in_BBox1 = Point_in_Rectangle((Xleft2, Ytop2), BBox1) and Point_in_Rectangle((Xleft2, Ybot2), BBox1) and Point_in_Rectangle((Xright2, Ytop2), BBox1) and Point_in_Rectangle((Xright2, Ybot2), BBox1) 
+    
+    # Check that for the intersection box, Xtop,Ytop is indeed on the top left of Xbot,Ybot
+    if BBox1_in_BBox2 or BBox2_in_BBox1:
+        #print('One BBox is included within the other')
+        IoU = 1
+    
+    elif Xright<Xleft or Ybot<Ytop : # it means that there is no intersection (bbox is inverted)
+        #print('No overlap')
+        IoU = 0 
+    
+    else:
+        # Compute area of the intersecting box
+        Inter = (Xright - Xleft + 1) * (Ybot - Ytop + 1) # +1 since we are dealing with pixels. See a 1D example with 3 pixels for instance
+        #print('Intersection area : ', Inter)
 
-		# Compute area of the union as Sum of the 2 BBox area - Intersection
-		Union = Width1 * Height1 + Width2 * Height2 - Inter
-		#print('Union : ', Union)
-		
-		# Compute Intersection over union
-		IoU = Inter/Union
-	
-	#print('IoU : ',IoU)
-	return IoU
+        # Compute area of the union as Sum of the 2 BBox area - Intersection
+        Union = Width1 * Height1 + Width2 * Height2 - Inter
+        #print('Union : ', Union)
+        
+        # Compute Intersection over union
+        IoU = Inter/Union
+    
+    #print('IoU : ',IoU)
+    return IoU
 
 
 
-def NMS(List_Hit, scoreThreshold=None, sortDescending=True, N_object=float("inf"), maxOverlap=0.5):
-	'''
-	Perform Non-Maxima supression : it compares the hits after maxima/minima detection, and removes the ones that are too close (too large overlap)
-	This function works both with an optionnal threshold on the score, and number of detected bbox
+def NMS(tableHit, scoreThreshold=None, sortDescending=True, N_object=float("inf"), maxOverlap=0.5):
+    '''
+    Perform Non-Maxima supression : it compares the hits after maxima/minima detection, and removes the ones that are too close (too large overlap)
+    This function works both with an optionnal threshold on the score, and number of detected bbox
 
-	if a scoreThreshold is specified, we first discard any hit below/above the threshold (depending on sortDescending)
-	if sortDescending = True, the hit with score below the treshold are discarded (ie when high score means better prediction ex : Correlation)
-	if sortDescending = False, the hit with score above the threshold are discared (ie when low score means better prediction ex : Distance measure)
+    if a scoreThreshold is specified, we first discard any hit below/above the threshold (depending on sortDescending)
+    if sortDescending = True, the hit with score below the treshold are discarded (ie when high score means better prediction ex : Correlation)
+    if sortDescending = False, the hit with score above the threshold are discared (ie when low score means better prediction ex : Distance measure)
 
-	Then the hit are ordered so that we have the best hits first.
-	Then we iterate over the list of hits, taking one hit at a time and checking for overlap with the previous validated hit (the Final Hit list is directly iniitialised with the first best hit as there is no better hit with which to compare overlap)	
-	
-	This iteration is terminate once we have collected N best hit, or if there are no more hit left to test for overlap 
+    Then the hit are ordered so that we have the best hits first.
+    Then we iterate over the list of hits, taking one hit at a time and checking for overlap with the previous validated hit (the Final Hit list is directly iniitialised with the first best hit as there is no better hit with which to compare overlap)    
+    
+    This iteration is terminate once we have collected N best hit, or if there are no more hit left to test for overlap 
    
    INPUT
     - tableHit         : (Panda DataFrame) Each row is a hit, with columns "TemplateName"(String),"BBox"(x,y,width,height),"Score"(float)
