37accd76f7c4fba48f7ded46889ed356b1b720fd
Laurent Thomas <laurent132.thomas@laposte.net>
2021-10-05 10:03:55 -0500

    more readable formating

--- test.py
+++ test.py
@@ -12,13 +12,16 @@ print(MTM.__version__)
 
 #%% Get image and templates by cropping
 image     = coins()
+
 smallCoin = image[37:37+38, 80:80+41]
 bigCoin   = image[14:14+59,302:302+65]
+listTemplates = [('small', smallCoin), 
+                 ('big', bigCoin)]
 
 
 #%% Perform matching
-tableHit = MTM.matchTemplates([('small', smallCoin), ('big', bigCoin)], image, score_threshold=0.3, method=cv2.TM_CCOEFF_NORMED, maxOverlap=0)  # Correlation-score
-#tableHit = MTM.matchTemplates([('small', smallCoin), ('big', bigCoin)], image, score_threshold=0.4, method=cv2.TM_SQDIFF_NORMED, maxOverlap=0) # Difference-score
+tableHit = MTM.matchTemplates(listTemplates, image, score_threshold=0.3, method=cv2.TM_CCOEFF_NORMED, maxOverlap=0)  # Correlation-score
+#tableHit = MTM.matchTemplates(listTemplates, image, score_threshold=0.4, method=cv2.TM_SQDIFF_NORMED, maxOverlap=0) # Difference-score
 
 print("Found {} coins".format(len(tableHit)))
 print(tableHit)
